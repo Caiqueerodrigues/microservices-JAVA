fc81d06eaf6a92bce1998045a9c60df9eceea2c4
Caíque Rodrigues <caiquerodriguesalmeida@gmail.com>
2025-02-03 19:01:13 -0300

    ajuste na criação da tabela

--- pagamentos/src/main/resources/db/migration/V1__create-table-pagamento.sql
+++ pagamentos/src/main/resources/db/migration/V1__create-table-pagamento.sql
@@ -1,11 +1,12 @@
 CREATE TABLE pagamentos(
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    id BIGINT AUTO_INCREMENT,
     valor DECIMAL(19, 2) NOT NULL,
     nome VARCHAR(100) NOT NULL,
     numero VARCHAR(19) NOT NULL,
-    expiracao VARCHAR(7) NOT NULL,,
+    expiracao VARCHAR(7) NOT NULL,
     codigo VARCHAR(3) NOT NULL,
     status VARCHAR(255) NOT NULL,
-    pedidoId BIGINT(20) NOT NULL,
-    formaDePagamento BIGINT(20) NOT NULL,
-)
+    pedido_id BIGINT(20) NOT NULL,
+    forma_de_pagamento_id BIGINT(20) NOT NULL,
+    PRIMARY KEY (id)
+);
