0c6a7e1e28b5a7206868ce7556e1434f92e0904b
Caíque Rodrigues <caiquerodriguesalmeida@gmail.com>
2025-02-03 17:42:26 -0300

    Criado os metodos getByID, getAll e create pagamento

--- src/main/java/com/exampleMicroServicos/services/PagamentoService.java
+++ src/main/java/com/exampleMicroServicos/services/PagamentoService.java
@@ -1,8 +1,17 @@
 package com.exampleMicroServicos.services;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import javax.persistence.EntityNotFoundException;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
+import com.exampleMicroServicos.DTO.PagamentoDto;
+import com.exampleMicroServicos.Enums.Status;
+import com.exampleMicroServicos.pagamentos.model.Pagamento;
 import com.exampleMicroServicos.repository.PagamentosRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -15,4 +24,27 @@ public class PagamentoService {
 
     private final ModelMapper modelMapper; 
     //necessario para validação dos dados que irão entrar e sair pelo DTO, pra fazer as validações dos dados
+
+    public List<PagamentoDto> getAll() {
+        return repository
+            .findAll()
+            .stream()
+            .map(p -> modelMapper.map(p, PagamentoDto.class)) //converte cada item da lista em um formato valido do DTO
+            .collect(Collectors.toList());
+    }
+
+    public PagamentoDto getByID(Long id) {
+        Optional<Pagamento> OptionalPagamento = repository.findById(id);
+
+        Pagamento pagamento = OptionalPagamento.orElseThrow(EntityNotFoundException::new);
+        return modelMapper.map(pagamento, PagamentoDto.class);
+    }
+
+    public PagamentoDto createPayment(PagamentoDto dto) {
+        Pagamento pagamento = modelMapper.map(dto, Pagamento.class);
+        pagamento.setStatus(Status.CRIADO);
+        repository.save(pagamento);
+
+        return modelMapper.map(pagamento, PagamentoDto.class);
+    }
 }
